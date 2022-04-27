ea69081f030f38eeafe8349e0339a14659055490
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-04-27 16:47:03 +0530

    Add memory leak detection

---
+++ FeedLoader/FeedAPI/FeedItemMapper.swift
@@ -0,0 +1,39 @@
+//
+//  FeedItemMapper.swift
+//  FeedLoader
+//
+//  Created by AmritPandey on 27/04/22.
+//
+
+import Foundation
+
+
+final class FeedItemsMapper {
+    
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Equatable, Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageUrl: image)
+        }
+        
+    }
+    
+    static var OK_200: Int {
+        return 200
+    }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
+    }
+}
--- FeedLoader/FeedAPI/RemoteFeedLoader.swift
+++ FeedLoader/FeedAPI/RemoteFeedLoader.swift
@@ -45,37 +45,17 @@ class RemoteFeedLoader {
             }
         }
     }
-
-}
-
-private class FeedItemsMapper {
     
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Equatable, Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageUrl: image)
+    private func map(_ data: Data, _ response: HTTPURLResponse)  -> Result {
+        do{
+            let items = try FeedItemsMapper.map(data, response)
+            return .success(items)
         }
-        
-    }
-    
-    static var OK_200: Int {
-        return 200
-    }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
+        catch {
+            return .failure(.invalidData)
         }
-        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
     }
+
 }
 
 
