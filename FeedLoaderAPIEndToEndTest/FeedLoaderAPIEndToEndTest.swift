2a25b24a978c58d54a917bb1495877d1f0cbfdc2
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-03 15:50:41 +0530

    For removing cache add ephemeral in session configuration

--- FeedLoaderAPIEndToEndTest/FeedLoaderAPIEndToEndTest.swift
+++ FeedLoaderAPIEndToEndTest/FeedLoaderAPIEndToEndTest.swift
@@ -26,8 +26,9 @@ class FeedLoaderAPIEndToEndTests: XCTestCase {
     //MARK: - Helpers
     
     func getFeedResult(file: StaticString = #file, line: UInt = #line) -> RemoteFeedLoader.Result? {
-        let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHttpClient()
+        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
+        let urlSession = URLSession(configuration: URLSessionConfiguration.ephemeral)
+        let client = URLSessionHttpClient(urlSession: urlSession)
         let loader = RemoteFeedLoader(url: url, client: client)
         trackMemoryLeak(instance: client, file: file, line: line)
         trackMemoryLeak(instance: loader, file: file, line: line)
@@ -37,7 +38,7 @@ class FeedLoaderAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10.0)
         return receivedResult
     }
 
