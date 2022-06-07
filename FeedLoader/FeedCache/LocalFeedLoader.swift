fa7da75c84606dee5bc784912bd8c09b2b20a5c5
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-07 16:18:54 +0530

    Add SaveResult type alias to protect future type failure

--- FeedLoader/FeedCache/LocalFeedLoader.swift
+++ FeedLoader/FeedCache/LocalFeedLoader.swift
@@ -7,17 +7,19 @@
 
 import Foundation
 
-class LocalFeedLoader {
+final class LocalFeedLoader {
 
     private let store: FeedStore
     private let currentDate: () -> Date
+    
+    typealias SaveResult = Error?
     
     init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
     
-    func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+    func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
         store.deleteCache { [weak self] error in
             guard let self = self else { return }
             if let cacheDeletionError = error {
@@ -29,7 +31,7 @@ class LocalFeedLoader {
         }
     }
     
-    private func cacheItem(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    private func cacheItem(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         store.insertCache(with: items, timestamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
             completion(error)
