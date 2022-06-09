993509f728151d21f4842d84bc0136e9763df727
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-09 16:50:40 +0530

    LocalFeedLoader does not message upon creation

---
+++ FeedLoaderTests/Feed cache/ValidateFeedCacheUseTestCaseFiles.swift
@@ -0,0 +1,27 @@
+//
+//  ValidateFeedCacheUseTestCaseFiles.swift
+//  FeedLoaderTests
+//
+//  Created by AmritPandey on 09/06/22.
+//
+
+import XCTest
+@testable import FeedLoader
+
+class ValidateFeedCacheUseTestCaseFiles: XCTestCase {
+
+    func test_init_doesnotHaveAnyMessageUponCreation() {
+        let (store, _) = makeSUT()
+        XCTAssertEqual(store.recievedMessages, [])
+    }
+
+    //MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (FeedStoreSpy, LocalFeedLoader){
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackMemoryLeak(instance: store, file: file, line: line)
+        trackMemoryLeak(instance: sut, file: file, line: line)
+        return(store, sut)
+    }
+}
