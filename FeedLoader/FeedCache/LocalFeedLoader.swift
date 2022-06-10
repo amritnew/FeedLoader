251b1cdbe26688532070c8500009275e09bed50d
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-10 14:37:47 +0530

    Made FeedacachePolicy Sattic as it doesn't manage state

--- FeedLoader/FeedCache/LocalFeedLoader.swift
+++ FeedLoader/FeedCache/LocalFeedLoader.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
-final class FeedCachePolicy {
-    private let maxCacheAgeInDays = 7
-    private let calendar = Calendar(identifier: .gregorian)
+private final class FeedCachePolicy {
+    private init() {}
     
-    func validate(_ timestamp: Date, against date: Date) -> Bool {
+    private static let maxCacheAgeInDays = 7
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
@@ -23,7 +25,6 @@ final class LocalFeedLoader {
 
     private let store: FeedStore
     private let currentDate: () -> Date
-    private let cachePolicy = FeedCachePolicy()
     
     init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -39,7 +40,7 @@ extension LocalFeedLoader: FeedLoader {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
             switch result {
-            case let .found(feeds, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(feeds, timestamp) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(feeds.toModels()))
                 
             case let .failure(error):
@@ -58,7 +59,7 @@ extension LocalFeedLoader {
             switch result {
             case .failure:
                 self.store.deleteCache {_ in }
-            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_, timestamp) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCache {_ in }
             case .empty, .found:
                 break
