840c5d0151fca431a28fd2b91c69a9aefdb4390a
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-13 15:23:02 +0530

    Extract SUT to helper method

--- FeedLoaderTests/Feed cache/CodableFeedStoreTests.swift
+++ FeedLoaderTests/Feed cache/CodableFeedStoreTests.swift
@@ -79,7 +79,7 @@ class CodableFeedStoreTests: XCTestCase {
     
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for cache retieval")
         sut.retrieve { result in
@@ -96,7 +96,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for cache retieval")
         sut.retrieve { firstResult in
@@ -115,7 +115,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValue() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueImageFeed().localModels
         let timestamp = Date()
         
@@ -138,6 +138,11 @@ class CodableFeedStoreTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+     
+    //MARK: Helpers
     
+    private func makeSUT() -> CodableFeedStore{
+        return CodableFeedStore()
+    }
     
 }
