f1bca447e760765644a61e1d75c40e15b4b3304c
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-20 17:06:31 +0530

    Add documentation to HTTClient

--- FeedLoader/FeedAPI/HttpClient.swift
+++ FeedLoader/FeedAPI/HttpClient.swift
@@ -13,6 +13,8 @@ enum HTTPClientResult {
 }
 
 protocol HTTPClient {
+    /// The Completion Handler can be invoked in any thread
+    /// Client are responsible to dispatchto appropriate thread. if needed
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
