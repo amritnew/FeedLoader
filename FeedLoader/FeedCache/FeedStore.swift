fa00e232c65c7c59b3cd884a6fed61de3fcd385f
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-20 17:05:50 +0530

    Add documentation to Feed store

--- FeedLoader/FeedCache/FeedStore.swift
+++ FeedLoader/FeedCache/FeedStore.swift
@@ -17,8 +17,16 @@ protocol FeedStore {
     typealias Completion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrievalCachedFeedResult) -> Void
     
+    /// The Completion Handler can be invoked in any thread
+    /// Client are responsible to dispatchto appropriate thread. if needed
     func retrieve(completion: @escaping RetrievalCompletion)
+    
+    /// The Completion Handler can be invoked in any thread
+    /// Client are responsible to dispatchto appropriate thread. if needed
     func deleteCache(completion: @escaping Completion)
+    
+    /// The Completion Handler can be invoked in any thread
+    /// Client are responsible to dispatchto appropriate thread. if needed
     func insertCache(with items: [LocalFeedImage], timestamp: Date, completion: @escaping Completion)
 }
 
