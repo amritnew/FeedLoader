87a28ac8c47a646dfeb3379b267a09f51fc07b5d
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-22 14:17:06 +0530

    Load persistence store coordinator in CoreDataFeedStore

--- FeedLoader/FeedCache/CoreDataFeedStore.swift
+++ FeedLoader/FeedCache/CoreDataFeedStore.swift
@@ -6,8 +6,15 @@
 //
 
 import Foundation
+import CoreData
 
 final class CoreDataFeedStore: FeedStore {
+    
+    private let container: NSPersistentContainer
+    
+    init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
     
     func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
@@ -21,7 +28,35 @@ final class CoreDataFeedStore: FeedStore {
         
     }
     
+}
+
+extension NSPersistentContainer {
     
+    enum LoadingError: Error {
+        case modelNotFound
+        case failedToLoadPersistenceStore(Error)
+    }
+    
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistenceStore($0) }
+        return container
+    }
+}
+
+extension NSManagedObjectModel {
+    
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap{ NSManagedObjectModel(contentsOf: $0) }
+    }
 }
 
 
