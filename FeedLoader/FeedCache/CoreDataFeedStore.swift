11d907c4b84bc47f1c515ca66d5914c89b61cb4a
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-06-22 12:21:22 +0530

    CoreaDataFeedStore deliver empty on empty cache

---
+++ FeedLoader/FeedCache/CoreDataFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataFeedStore.swift
+//  FeedLoader
+//
+//  Created by AmritPandey on 21/06/22.
+//
+
+import Foundation
+
+final class CoreDataFeedStore: FeedStore {
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    func deleteCache(completion: @escaping Completion) {
+        
+    }
+    
+    func insertCache(with items: [LocalFeedImage], timestamp: Date, completion: @escaping Completion) {
+        
+    }
+    
+    
+}
--- FeedLoaderTests/Feed cache/CoreDataFeedStoreTests.swift
+++ FeedLoaderTests/Feed cache/CoreDataFeedStoreTests.swift
@@ -11,7 +11,8 @@ import XCTest
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -56,5 +57,13 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_storeSodeEffects_runSerially() {
         
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackMemoryLeak(instance: sut, file: file, line: line)
+        return sut
     }
 }
