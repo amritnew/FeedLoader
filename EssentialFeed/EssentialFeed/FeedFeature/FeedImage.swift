0458e6813c3dd9a1701c402f862213b1568d5da7
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-07-12 15:11:35 +0530

    Replace "FeedViewControllerTest.LoaderSpy" make follow to "FeedLoader" production abstraction

--- EssentialFeed/EssentialFeed/FeedFeature/FeedImage.swift
+++ EssentialFeed/EssentialFeed/FeedFeature/FeedImage.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-struct FeedImage: Equatable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let url: URL
+public struct FeedImage: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
     
-    init(id: UUID, description: String?, location: String?, url: URL) {
+    public init(id: UUID, description: String?, location: String?, url: URL) {
         self.id = id
         self.description = description
         self.location = location
--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -7,17 +7,18 @@
 
 import XCTest
 import UIKit
+import EssentialFeed
 
 final class FeedViewController: UIViewController {
-    private var loader: FeedViewControllerTests.LoaderSpy?
+    private var loader: FeedLoader?
     
-    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+    convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
     
     override func viewDidLoad() {
-        loader?.load()
+        loader?.load{ _ in }
     }
 }
 
@@ -39,10 +40,11 @@ class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
-    class LoaderSpy {
+    class LoaderSpy: FeedLoader {
+        
         private(set) var loadCallCount = 0
         
-        func load() {
+        func load(completion: @escaping ((FeedLoader.Result) -> Void)) {
             loadCallCount += 1
         }
     }
