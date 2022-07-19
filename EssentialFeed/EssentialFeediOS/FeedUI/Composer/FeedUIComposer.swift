0ae39fa8896e67dac6d7b02f5b0ad6f668973ab1
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-07-19 15:27:35 +0530

    Extract adapter pattern to a separate function

--- EssentialFeed/EssentialFeediOS/FeedUI/Composer/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composer/FeedUIComposer.swift
@@ -14,12 +14,16 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         let feedViewController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = { [weak feedViewController] feed in
+        refreshController.onRefresh = adaptFeedToCellControllers(forwradingTo: feedViewController, imageLoader: imageLoader)
+        
+        return feedViewController
+    }
+    
+    private static func adaptFeedToCellControllers(forwradingTo feedViewController:FeedViewController, imageLoader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+        return { [weak feedViewController] feed in
             feedViewController?.tableModel = feed.map({ feed in
                 return FeedImageCellController(model: feed, imageLoader: imageLoader)
             })
         }
-        
-        return feedViewController
     }
 }
